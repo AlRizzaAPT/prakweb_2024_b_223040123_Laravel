1057afa3261d1e9dc5505cf40effc89fcdc7283b
AlRizzaAchmad <acmadal@gmail.com>
2024-11-03 18:49:14 +0700

    Video 4 laravel 11

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman contact</title>
 </head>
 <body>
     <a href="/">Home</a>
     <a href="/blog">Blog</a>
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
-    <h1>Halaman About</h1>
-    <h3>owner: {{$nama}}</h3>
-    <img src="img/asus.jpg" alt="asus" width="200">
-    
+    <h1>halaman Contact</h1>
+    <ul>
+        <li>https://www.youtube.com/watch?v=vDx6VA-6a6Y&list=PLFIM0718LjIW1Xb7cVj7LdAr32ATDQMdr&index=4</li>
+        <li>https://www.youtube.com/watch?v=vDx6VA-6a6Y&list=PLFIM0718LjIW1Xb7cVj7LdAr32ATDQMdr&index=4</li>
+    </ul>
 </body>
 </html>
