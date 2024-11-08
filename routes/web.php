9ca6f2bc9bb3727c8bb87d9ae135ae8cfb6aaf23
AlRizzaAchmad <acmadal@gmail.com>
2024-11-08 12:24:13 +0700

    Video 7 laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -14,53 +16,19 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Riza',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est odit laudantium earum cumque pariatur facilis vitae nulla esse quasi accusantium! Est iste facilis magnam dolor soluta, maxime quo architecto earum!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Riza',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate esse architecto dolorum dicta accusamus fugit id culpa atque, aut explicabo a voluptatibus sit mollitia ipsam natus quod nostrum nobis ut.'
-            ]
-        ]
+        'posts' => Post::all()
     ]);
 });
 
-// Route for a single post, e.g. /posts/{slug}
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Riza',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est odit laudantium earum cumque pariatur facilis vitae nulla esse quasi accusantium! Est iste facilis magnam dolor soluta, maxime quo architecto earum!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Riza',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate esse architecto dolorum dicta accusamus fugit id culpa atque, aut explicabo a voluptatibus sit mollitia ipsam natus quod nostrum nobis ut.'
-        ]
-    ];
+    $posts = Post::all();
 
-    // Use the slug to find the post
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;  // Compare with the slug
-    });
+    $post = Post ::find($slug);
 
     if ($post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
     } else {
-        return redirect('/posts'); // Redirect to the list of posts if post is not found
+        return redirect('/posts');
     }
 });
 
