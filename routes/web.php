dc2dcd1905d65c6479a685af3e63690d456e6037
AlRizzaAchmad <acmadal@gmail.com>
2024-11-09 16:55:47 +0700

    Video 12 laravel 11

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,50 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
+    return view('home', [
+        'title' => 'Home Page'
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Riza', 'title' => 'About']);
+    return view('about', [
+        'nama' => 'Riza',
+        'title' => 'About'
+    ]);
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    return view('posts', [
+        'title' => 'Blog',
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
- 
-
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
     return view('posts', [
-        'title' => "Articles by $user->name",
+        'title' => count($user->posts) . ' Articles by ' . $user->name,
         'posts' => $user->posts
     ]);
 });
 
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
+});
+
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+    return view('contact', [
+        'title' => 'Contact'
+    ]);
 });
