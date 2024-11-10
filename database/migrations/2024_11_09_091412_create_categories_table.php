27b251cf8eb3afc4da781525e5dd03e9e89efacd
AlRizzaAchmad <acmadal@gmail.com>
2024-11-10 12:19:09 +0700

    Video 15 laravel 11

--- database/migrations/2024_11_09_091412_create_categories_table.php
+++ database/migrations/2024_11_09_091412_create_categories_table.php
@@ -12,10 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->timestamps();
+              $table->id();
+        $table->string('name');
+        $table->string('slug')->unique();
+        $table->string('color');  // Menambahkan kolom color
+        $table->timestamps();
         });
     }
 
